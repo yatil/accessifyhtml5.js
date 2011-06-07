6b75086c1d7324038bc74c09d46b547daa059d71
adickson <adickson@tscnsw.org.au>
2011-06-07 13:55:13 +0800

    JS only version (i.e. without JQuery) with a defaults object

--- accessifyhtml5.js
+++ accessifyhtml5.js
@@ -1,39 +1,73 @@
 /*
- * Accessifyhtml5.js
- * Adds ARIA to new elements in browsers which don’t do it by themselves.
- * Just drop into the bottom of your web page:
- * <script src="accessifyhtml5.js"></script>
- * 
- * Yes, it depends on jQuery.
- *
- * Souce: http://www.html5accessibility.com/index-aria.html
- *
- * Todo: Extend Script for other elements, probably even play with fallback JS for inaccessible audio/video.
- *
- * Acknowledgements: 
- * - @stevefaulkner for his work exploring html5 a11y,
- * - @paddya91 for object notation & document ready
- * - @ginader for reporting typo
- * - @webaxe for reporting an error
- */
+* Accessifyhtml5.js
+* Adds ARIA to new elements in browsers which don’t do it by themselves.
+* Just drop into the bottom of your web page:
+* <script src="accessify.js"></script>
+*
+* Souce: http://www.html5accessibility.com/index-aria.html
+*
+* Todo: Extend Script for other elements, probably even play with fallback JS for inaccessible audio/video.
+*
+* Acknowledgements:
+* - @stevefaulkner for his work exploring html5 a11y,
+* - @paddya91 for object notation & document ready
+* - @ginader for reporting typo
+* - @webaxe for reporting an error
+*/
 
-$(document).ready(function() {
-    
-    var fixes = {
-        'header.site'   : { 'role':          'banner'        },
-        'footer.site'   : { 'role':          'contentinfo'   },
-        'article'       : { 'role':          'article'       },
-        'aside'         : { 'role':          'complementary' },
-        'nav'           : { 'role':          'navigation'    },
-        'output'        : { 'aria-live':     'polite'        },
-        'section'       : { 'role':          'region'        },
-        '[required]'    : { 'aria-required': 'true'          }
-    };
+/* 
+* JS only version by adickson 
+* 
+*  You can specify an object containing default header and footer CSS selector properties when you call AccessifyHTML5
+*  i.e. 
+*    AccessifyHTML5({header:"#header", footer:"#footer"});
+*
+*/
 
-    $.each(fixes,
-        function(index, item) {
-            $(index).attr(item);
+
+var AccessifyHTML5 = function (defaults) {
+
+        var fixes = {
+            'header'    :    {'role':  'banner'        },
+            'footer'    :    {'role':  'contentinfo'   },
+            'article'   :    {'role':  'article'       },
+            'aside'     :    {'role':  'complementary' },
+            'nav'       :    {'role':  'navigation'    },
+            'output'    :    {'aria-live': 'polite'    },
+            'section'   :    {'role':  'region'        },
+            '[required]':    {'aria-required': 'true'  }
+        };
+		
+        if (defaults) {
+            if (defaults.header) {
+                delete fixes['header'];
+                fixes[defaults.header] = {
+                    'role': 'banner'
+                };
+            }
+            if (defaults.footer) {
+                delete fixes['footer'];
+                fixes[defaults.footer] = {
+                    'role': 'contentinfo'
+                }
+            }
         }
-    );
-    
-});
+
+        for (fix in fixes) {
+
+            var elems = document.querySelectorAll(fix),
+                obj = fixes[fix],
+                attr, value, i = 0;
+
+            for (key in obj) {
+                attr = key;
+                value = obj[key];
+            }
+
+            for (i; i < elems.length; i++) {
+                elems[i].setAttribute(attr, value);
+            }
+
+        }
+
+    };
