16942014587098b3bfb385785dd56b76fce64d71
Eric Eggert <eric.eggert@outlinewebdesign.com>
2011-08-15 00:58:52 +0200

    Non jQuery solution, better jQuery solution, attribution

--- accessifyhtml5.js
+++ accessifyhtml5.js
@@ -1,35 +1,30 @@
 /*
-* Accessifyhtml5.js
-* Adds ARIA to new elements in browsers which don’t do it by themselves.
-* Just drop into the bottom of your web page:
-* <script src="accessify.js"></script>
-*
-* Souce: http://www.html5accessibility.com/index-aria.html
-*
-* Todo: Extend Script for other elements, probably even play with fallback JS for inaccessible audio/video.
-*
-* Acknowledgements:
-* - @stevefaulkner for his work exploring html5 a11y,
-* - @paddya91 for object notation & document ready
-* - @ginader for reporting typo
-* - @webaxe for reporting an error
+ * Accessifyhtml5.js
+ * Adds ARIA to new elements in browsers which don’t do it by themselves.
+ * Just drop into the bottom of your web page:
+ * <script src="accessify.js"></script>
+ *
+ * Souce: http://www.html5accessibility.com/index-aria.html
+ *
+ * New in this version:
+ * – Added non-jQuery version (Using querySelector, adds support in IE8+, FF3.5+, Opera 10+, Safari, Chrome as well as iOS, Android and Opera Mobile)
+ * – Removed header.site getting role banner and footer.site getting role contentinfo -> If you need to add a class, you can also add the role directly to the HTML
+ * – Thanks to @adickson you can just add selectors for banner and contentinfo when calling the function
+ * – You’ll now have to call AccessifyHTML5-function like that
+ *   AccessifyHTML5({header:"#header", footer:"#footer"});
+ *
+ * Acknowledgements:
+ * - @adickson for a really good non-jquery solution
+ * - @stevefaulkner for his work exploring html5 a11y
+ * - @paddya91 for object notation & document ready
+ * - @ginader for reporting typo
+ * - @webaxe for reporting an error
+ * – @divya for keeping me on track for a non-js solution
 */
-
-/* 
-* JS only version by adickson 
-* 
-*  You can specify an object containing default header and footer CSS selector properties when you call AccessifyHTML5
-*  i.e. 
-*    AccessifyHTML5({header:"#header", footer:"#footer"});
-*
-*/
-
 
 var AccessifyHTML5 = function (defaults) {
-
+    if(document.querySelectorAll) {
         var fixes = {
-            'header'    :    {'role':  'banner'        },
-            'footer'    :    {'role':  'contentinfo'   },
             'article'   :    {'role':  'article'       },
             'aside'     :    {'role':  'complementary' },
             'nav'       :    {'role':  'navigation'    },
@@ -40,13 +35,11 @@ var AccessifyHTML5 = function (defaults) {
 		
         if (defaults) {
             if (defaults.header) {
-                delete fixes['header'];
                 fixes[defaults.header] = {
                     'role': 'banner'
                 };
             }
             if (defaults.footer) {
-                delete fixes['footer'];
                 fixes[defaults.footer] = {
                     'role': 'contentinfo'
                 }
@@ -69,5 +62,5 @@ var AccessifyHTML5 = function (defaults) {
             }
 
         }
-
-    };
+    }
+};
