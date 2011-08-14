502be494c3ae369b6cbdc98c6b3a19db73ac4a58
Eric Eggert <eric.eggert@outlinewebdesign.com>
2011-08-14 18:25:02 +0200

    Revert "Added the solution @divya provided in the HTML5Boilerplate issue thread, removed header.site, footer.site"
    This reverts commit 6c4cd6255fa4d9a475e34c466008db000b6cb5f3.

--- accessifyhtml5.js
+++ accessifyhtml5.js
@@ -3,37 +3,37 @@
  * Adds ARIA to new elements in browsers which don’t do it by themselves.
  * Just drop into the bottom of your web page:
  * <script src="accessifyhtml5.js"></script>
+ * 
+ * Yes, it depends on jQuery.
  *
  * Souce: http://www.html5accessibility.com/index-aria.html
  *
- * New in this version:
- * – Added non-jQuery version (Using querySelector, adds support in IE8+, FF3.5+, Opera 10+, Safari, Chrome as well as iOS, Android and Opera Mobile)
- * – Removed header.site getting role banner and footer.site getting role contentinfo -> If you need to add a class, you can also add the role directly to the HTML
+ * Todo: Extend Script for other elements, probably even play with fallback JS for inaccessible audio/video.
  *
  * Acknowledgements: 
  * - @stevefaulkner for his work exploring html5 a11y,
  * - @paddya91 for object notation & document ready
  * - @ginader for reporting typo
  * - @webaxe for reporting an error
- * – @divya for the non-jQuery version: https://github.com/paulirish/html5-boilerplate/issues/274#issuecomment-1399875
  */
 
-// Use this format to add to this list: 
-// <selector> | <aria-attribute-name> | <aria-attribute-value>
+$(document).ready(function() {
+    
+    var fixes = {
+        'header.site'   : { 'role':          'banner'        },
+        'footer.site'   : { 'role':          'contentinfo'   },
+        'article'       : { 'role':          'article'       },
+        'aside'         : { 'role':          'complementary' },
+        'nav'           : { 'role':          'navigation'    },
+        'output'        : { 'aria-live':     'polite'        },
+        'section'       : { 'role':          'region'        },
+        '[required]'    : { 'aria-required': 'true'          }
+    };
 
-var aria = [
-    'article | role | article',
-    'aside | role | complementary',
-    'nav | role | navigation',
-    'output | aria-live | polite',
-    'section | role | region',
-    '[required] | aria-required | true'
-];
-
-aria.forEach(function(value, index) {
-    var ariarow = value.split(' | ');
-    var elm = document.querySelector(ariarow[0])
-    if (elm) {
-        elm.setAttribute(ariarow[1], ariarow[2]);
-    }
+    $.each(fixes,
+        function(index, item) {
+            $(index).attr(item);
+        }
+    );
+    
 });
